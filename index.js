0c098c169baed9da69bb7e94243a11a134bc7370
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-01-21 17:46:52 -0500

    Adding places controllers

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 require('dotenv').config()
-
 const app = express()
+
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.send('hello world')
