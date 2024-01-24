43a9c38d34dd8736a96e6c0b0f3b5ee7166f2226
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-01-23 20:34:01 -0500

    added bootstrap and custom css stylesheet

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use('/places', require('./controllers/places'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.render('home')
