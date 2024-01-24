5824fcb3d0854b05f86d485b3c8f3619b0434391
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-01-24 13:29:24 -0500

    made submission page but failing to POST

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,35 @@ function new_form() {
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                <form action="/places" method='POST'>
+                    <div>
+                        <label htmlFor="name">Place Name</label>
+                        <input id='name' name='name' required/>
+                    </div>
+                    <div>
+                        <label htmlFor="pic">Place Picture</label>
+                        <input type="url" id='pic'
+                        name='pic'/>
+                    </div>
+                    <div>
+                        <label htmlFor="city">City</label>
+                        <input type="text" id='city'
+                        name='city'/>
+                    </div>
+                    <div>
+                        <label htmlFor="state">State</label>
+                        <input type="text" id='state'
+                        name='state'/>
+                    </div>
+                    <div>
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input type="text" id='cuisines'
+                        name='cuisines'required/>
+                    </div>
+                    <div>
+                        <input type="submit" value="Add Place"/>
+                    </div>
+                </form>
             </main>
         </Def>
     )
