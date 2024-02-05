514ac62a0b66cbb683959e478ff92fd51c7e6a46
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-02-04 22:05:45 -0500

    added error message for invalid founded year

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,11 +1,21 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form() {
+function new_form(data) {
+    let message = ''
+        if(data.message){
+            message = (
+                <h4 className='alert alert-warning' role='alert'>
+                    {data.message}
+                </h4>
+            )
+        }
+
     return (
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                {message}
                 <form style={{margin: '7px'}} action="/places" method='POST'>
                     <div className='row'>
                     <div className='form-group col-sm-6'>
@@ -16,7 +26,8 @@ function new_form() {
                         <label htmlFor="pic">Place Picture</label>
                         <input className='form-control' type="text" id='pic'
                         name='pic'
-                        pattern="https?://.+" title="Include http://"/>
+                        pattern="https?://.+" title="Include http://"
+                        defaultValue={'https://images.unsplash.com/photo-1628260412297-a3377e45006f?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}/>
                     </div>
                     <div className='form-group col-sm-6'>
                         <label htmlFor="city">City</label>
@@ -35,7 +46,7 @@ function new_form() {
                     </div>
                     <div className='form-group'>
                         <label htmlFor="founded">Founded Year</label>
-                        <input className="form-control" id='founded' name='founded'/>
+                        <input type='number' className="form-control" id='founded' name='founded' value={new Date().getFullYear()}/>
                     </div>
                     <div style={{margin: 'auto'}}>
                         <input style={{backgroundColor: '#057746', color: 'white'}} className='btn' type="submit" value="Add Place"/>
